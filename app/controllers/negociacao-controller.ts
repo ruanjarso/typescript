9d20f9292dee345ec9373a479beb80227c46ca8c
Ruan Jarso <105135491+ruanjarso@users.noreply.github.com>
2024-02-21 17:09:28 -0300

    organizando o código e tipando os métodos

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -12,14 +12,17 @@ export class NegociacaoController {
         this.inputValor = document.querySelector("#valor")
     }
 
-    adiciona(){
+    adiciona(): void{
+        const negociacao =  this.criaNegociacao();
+        console.log(negociacao);
+    }
+    
+    criaNegociacao(): Negociacao{
         const exp = /-/g;     //expressão regular: é basicamente o que eu quero que seja substituido pela "," que no caso é o '-' 
         const date = new Date(this.inputData.value.replace(exp, ','))    // o replace vai trocar o que tem na minha expressão regular(exp) pela proximo vslor do parametro. 
         const quantidade = parseInt(this.inputQuantidade.value);    // convertendo o valor de "quantidade" para Inteiro
         const valor = parseFloat(this.inputValor.value)    // convertendo o valor de "valor" para Float
-  
-        const negociacao = new Negociacao(date,quantidade,valor);
-
-        console.log(negociacao);
+    
+        return new Negociacao(date,quantidade,valor);
     }
 }
